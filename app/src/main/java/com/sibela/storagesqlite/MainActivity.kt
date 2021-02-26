2246bd9b5d382cc3911e6d7a26725e6b9ba63740
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-26 14:23:39 -0300

    Implementing CRUD

--- app/src/main/java/com/sibela/storagesqlite/MainActivity.kt
+++ app/src/main/java/com/sibela/storagesqlite/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.sibela.storagesqlite
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.editButton
 
 class MainActivity : AppCompatActivity() , TaskAdapter.Callback {
 
     private var selectedTask: Task? = null
     private lateinit var adapter: TaskAdapter
+    private lateinit var taskDao: TaskDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        taskDao = TaskDaoSqlite(this)
 
         setupRecyclerView()
         saveButton.setOnClickListener { onSaveClicked() }
@@ -21,15 +24,25 @@ class MainActivity : AppCompatActivity() , TaskAdapter.Callback {
     }
 
     override fun onEditClicked(task: Task) {
-        editButton.visibility = View.VISIBLE
-        saveButton.visibility = View.INVISIBLE
+        displayEditMode()
         taskInput.setText(task.name)
         this.selectedTask = task
+    }
+
+    private fun displayEditMode() {
+        editButton.visibility = View.VISIBLE
+        saveButton.visibility = View.INVISIBLE
+    }
+
+    private fun displaySaveMode() {
+        editButton.visibility = View.INVISIBLE
+        saveButton.visibility = View.VISIBLE
     }
 
     private fun setupRecyclerView() {
         adapter = TaskAdapter(callback = this)
         val tasks = arrayListOf<Task>()
+        tasks.addAll(taskDao.fetchAll())
         adapter.setTaskArray(tasks)
         taskRecycler.apply {
             setHasFixedSize(true)
@@ -43,6 +56,10 @@ class MainActivity : AppCompatActivity() , TaskAdapter.Callback {
         saveTask(task)
     }
 
+    private fun clearForm() {
+        taskInput.setText("")
+    }
+
     private fun onEditClicked() {
         val editedTask = Task(selectedTask!!.id, taskInput.text.toString())
         editTask(editedTask)
@@ -50,14 +67,32 @@ class MainActivity : AppCompatActivity() , TaskAdapter.Callback {
     }
 
     override fun onDeleteClicked(task: Task) {
-
+        val removed = taskDao.remove(task)
+        if (removed) {
+            val tasksArray = arrayListOf<Task>()
+            tasksArray.addAll(taskDao.fetchAll())
+            adapter.setTaskArray(tasksArray)
+        }
     }
 
     private fun saveTask(task: Task) {
-
+        val inserted = taskDao.insert(task)
+        if (inserted) {
+            val tasksArray = arrayListOf<Task>()
+            tasksArray.addAll(taskDao.fetchAll())
+            adapter.setTaskArray(tasksArray)
+            clearForm()
+        }
     }
 
     private fun editTask(task: Task) {
-
+        val updated = taskDao.update(task)
+        if (updated) {
+            val tasksArray = arrayListOf<Task>()
+            tasksArray.addAll(taskDao.fetchAll())
+            adapter.setTaskArray(tasksArray)
+            clearForm()
+            displaySaveMode()
+        }
     }
 }
